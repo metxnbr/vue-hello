1107c51130c515fcf194e18e1138e561502912e3
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2018-12-10 10:15:18 +0800

    environment config (#1)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Environment Variables and Modes</h2>
+    <p>version: {{version}}</p>
+    <p>node env: {{nodeEnv}}</p>
+    <p>api host: {{host}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  data: () => ({
+    version: process.env.VUE_APP_VERSION,
+    nodeEnv: process.env.NODE_ENV,
+    host: process.env.VUE_APP_API_HOST
+  })
+};
+</script>
+
